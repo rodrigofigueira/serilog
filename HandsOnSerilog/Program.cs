065c5e561a99837e599fbf5b977e8b42986866ef
Rodrigo Figueira <rodrigoservice@gmail.com>
2024-08-13 12:33:41 -0300

    Console and File Sample

---
+++ HandsOnSerilog/Program.cs
@@ -0,0 +1,10 @@
+﻿using Serilog;
+
+using var log = new LoggerConfiguration()
+                .WriteTo.Console() // Sinks.Console
+                .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day) // Sinks.File
+                .CreateLogger();
+
+log.Information("Hello, Serilog!");
+log.Error("Something went wrong");
+log.Debug("Debugging something");
